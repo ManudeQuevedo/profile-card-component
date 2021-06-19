7e0718c9c003ceb05d8daa93f49d2731d9534c6b
Manu de Quevedo <manudequevedo@gmail.com>
2021-06-19 13:25:07 -0500

    Starter react files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Components/Card/Card';
+import './App.css';
+
+function App() {
+   return (
+      <div className='App'>
+         <h1>Hello from React World!</h1>
+         <Card />
+      </div>
+   );
+}
+
+export default App;
---
+++ src/Components/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Card() {
+   return (
+      <React.Fragment>
+         <h2>This is the Card component!</h2>
+      </React.Fragment>
+   );
+}
+
+export default Card;
