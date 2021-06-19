fbe117cc612bcdcbedabe5dab60975c9d9e6a709
Manu de Quevedo <manudequevedo@gmail.com>
2021-06-19 15:09:18 -0500

    Work is still in progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Card from './Components/Card/Card';
+import { ReactComponent as BgPatternTop } from './images/bgPatternTop.svg';
+import { ReactComponent as BgPatternBottom } from './images/bgPatternBottom.svg';
 import './App.css';
 
 function App() {
    return (
       <div className='App'>
-         <h1>Hello from React World!</h1>
-         <Card />
+         <h1 className='App-title'>Hello from React World!</h1>
+         <Card className='Card' />
       </div>
    );
 }
